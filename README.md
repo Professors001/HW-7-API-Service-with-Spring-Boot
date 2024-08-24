0c9d4a52f54ed1cb3a4336696c7a5fdf3df7289f
BR0F3SS0R <aphisit.pra@ku.th>
2024-08-24 21:11:07 +0700

    SUCCESS

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # HW-7-API-Service-with-Spring-Boot
+## 6510451085 Aphisit Prasertvesyakorn
---
+++ src/test/java/ku/cs/restaurant/RestaurantApplicationTests.java
@@ -0,0 +1,14 @@
+package ku.cs.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+//6510451085 Aphisit Prasertvesyakorn
